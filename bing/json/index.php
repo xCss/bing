00ec5bed20c9195524b606906d6f6151fffb442f
heroyy <yy.liu@foxmail.com>
2016-04-05 20:10:44 +0800

    fix Access-Control-Allow-Origin

--- bing/json/index.php
+++ bing/json/index.php
@@ -5,6 +5,9 @@
 * Link https://api.ioliu.cn 
 * Follow https://github.com/eary/bing
 */
+header('Access-Control-Allow-Origin:*');  
+header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
+header('content-type: application/json; charset=utf-8');
 require('../../lib/base.php');
 $base = new Base();
 $array = $base->getLastPic();
