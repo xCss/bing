9f46752ac6ae3f6c545ee7a3853b9bf9e5961f8a
heroyy <yy.liu@foxmail.com>
2016-04-05 21:06:34 +0800

    给title添加页面

--- index.php
+++ index.php
@@ -16,19 +16,29 @@ $pics = $api -> getPicsByPagin($p);
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Pictrues | Bing Inc.</title>
+		<title>
+            <?php
+            if($p>1){
+                echo '第'.$p.'页 - 必应壁纸. ';
+            }else {
+                echo '首页 - 必应壁纸. ';
+            }
+            ?>
+              
+        </title>
 		<meta charset="utf-8" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-        <meta name="keywords" content="Bing,Bing壁纸,bing壁纸api,Bing最新壁纸,壁纸分享,壁纸下载">
+        <meta name="keywords" content="Bing,Bing壁纸,bing壁纸api,Bing最新壁纸,壁纸分享,壁纸下载,必应壁纸">
         <meta name="description" content="Bing每日壁纸:风景, 这边独好。">
 	    <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,minimum-scale=1,maximum-scale=1">
         <meta name="author" content="Bitmoe · eary">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="renderer" content="webkit">
         <meta name="theme-color" content="#4F4F4F"/><link href="/assets/css/images/bing.ico" rel="shortcut icon"/>
+        
     </head>
 	<body>
 
@@ -134,8 +144,6 @@ $(function(){
     s.parentNode.insertBefore(hm, s);
     
 });
-    
-
 </script>
 
 </body>
