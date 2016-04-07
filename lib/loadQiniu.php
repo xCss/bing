37e1a23e88c334437203adfa818eccd0a2238464
heroyy <yy.liu@foxmail.com>
2016-04-07 18:58:41 +0800

    test qiniu

--- lib/loadQiniu.php
+++ lib/loadQiniu.php
@@ -4,14 +4,19 @@ require_once 'autoload.php';
 use Qiniu\Auth;
 
 // 引入上传类
-use Qiniu\Storage\UploadManager;
+use Qiniu\Storage\BucketManager;
 
 class LoadQiniu{
     
     // 需要填写你的 Access Key 和 Secret Key
     var $accessKey = 'IuDKM4c-08H6FTvvxr9UAS3RV6-ILsDowpu0a_r3';
     var $secretKey = 'E6N95oHDlyNbLgmHZCPuBUE28pHwEoSTVVjj0YH7';
-
+    /*****
+    // 要上传文件的本地路径
+    //$filePath = './php-logo.png';
+    // 上传到七牛后保存的文件名
+    //$key = 'my-php-logo.png';
+    */
     function upload($filePath,$key){
         // 构建鉴权对象
         $auth = new Auth($accessKey, $secretKey);
@@ -20,19 +25,10 @@ class LoadQiniu{
         $bucket = 'ioliu';
 
         // 生成上传 Token
-        $token = $auth->uploadToken($bucket);
-
-        // 要上传文件的本地路径
-        //$filePath = './php-logo.png';
-
-        // 上传到七牛后保存的文件名
-        //$key = 'my-php-logo.png';
-
-        // 初始化 UploadManager 对象并进行文件的上传
-        $uploadMgr = new UploadManager();
-
+        $bucketMgr = $auth->BucketManager($auth);
+        
         // 调用 UploadManager 的 putFile 方法进行文件的上传
-        list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
+        list($ret, $err) = $uploadMgr->fetch($filePath,$bucket, $key);
         echo "\n====> putFile result: \n";
         if ($err !== null) {
             var_dump($err);
