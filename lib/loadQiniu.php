649aaf02a9d1e61475c58dae883ac02977741f7e
heroyy <yy.liu@foxmail.com>
2016-04-07 19:35:17 +0800

    test qiniu

--- lib/loadQiniu.php
+++ lib/loadQiniu.php
@@ -20,7 +20,7 @@ class LoadQiniu{
     function upload($filePath,$key){
         // 构建鉴权对象
         $auth = new Auth($accessKey, $secretKey);
-
+        echo $auth;
         // 要上传的空间
         $bucket = 'ioliu';
 
