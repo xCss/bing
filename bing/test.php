3380e679e29fbe92c9d7730b4c5016516cc6e5be
heroyy <yy.liu@foxmail.com>
2016-04-07 19:17:26 +0800

    test qiniu

--- bing/test.php
+++ bing/test.php
@@ -2,7 +2,7 @@
 
 require_once('../lib/loadQiniu.php');
 $qiniu = new LoadQiniu();
-$qiniu->upload('http://s.cn.bing.net/az/hprichbg/rb/RabbitIsland_ZH-CN10320047201_1920x1080.jpg','az/hprichbg/rb/RabbitIsland_ZH-CN10320047201_1920x1080.jpg');
+$qiniu->upload('http://s.cn.bing.net/az/hprichbg/rb/RabbitIsland_ZH-CN10320047201_1920x1080.jpg','RabbitIsland_ZH-CN10320047201_1920x1080.jpg');
 
 //phpinfo();
 ?>
