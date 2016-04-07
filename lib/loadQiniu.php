815975fbfc9ef656ecae81e325e61d4deffc8318
heroyy <yy.liu@foxmail.com>
2016-04-07 19:09:12 +0800

    test qiniu

--- lib/loadQiniu.php
+++ lib/loadQiniu.php
@@ -1,13 +1,13 @@
 <?php
 require_once 'autoload.php';
 // 引入鉴权类
-use Qiniu\Auth;
+use Qiniu\Auth as AUTH;
 
 // 引入上传类
-use Qiniu\Storage\BucketManager;
+use Qiniu\Storage\BucketManager as BKM;
+    
 
 class LoadQiniu{
-    
     // 需要填写你的 Access Key 和 Secret Key
     var $accessKey = 'IuDKM4c-08H6FTvvxr9UAS3RV6-ILsDowpu0a_r3';
     var $secretKey = 'E6N95oHDlyNbLgmHZCPuBUE28pHwEoSTVVjj0YH7';
@@ -19,12 +19,12 @@ class LoadQiniu{
     */
     function upload($filePath,$key){
         // 构建鉴权对象
-        $auth = new Auth($accessKey, $secretKey);
+        $auth = new AUTH\Auth($accessKey, $secretKey);
 
         // 要上传的空间
         $bucket = 'ioliu';
 
-        $bucketMgr = $auth->BucketManager($auth);
+        $bucketMgr = new BKM\BucketManager($auth);
         
         // 调用 BucketManager 的 fetch 方法进行文件的上传
         $items = $bucketMgr->fetch($filePath,$bucket, $key);
