99b60c7ec282096c7e52f1b6db1bdc1550ed3d61
eary <yy.liu@foxmail.com>
2016-05-12 11:51:35 +0800

    some fix

--- lib/config.bak.php
+++ lib/config.bak.php
@@ -6,7 +6,7 @@
 */
 /*******************七牛相关********************/
 define( 'QINIUCDN' , 'https://dn-ioliu.qbox.me/bing/');
-define( 'QINIUAK' , 'e8qjVjHZTMUgNFKV6FevcjIRy8Ld99V-z2Io0trS');
-define( 'QINIUSK' , 'L6_wdVZEcU_Krf8aF6f9g8hfeFDfliWBhX-AGfLi');
+define( 'QINIUAK' , '*******************************');
+define( 'QINIUSK' , '*******************************');
 
 ?>
--- lib/db.bak.php
+++ lib/db.bak.php
@@ -10,13 +10,14 @@ class DBHelper{
     public function DBHelper(){}
     
     private static function getConn(){
-        $conn = mysqli_connect("qdm16395144.my3w.com","qdm16395144","abc123456","qdm16395144_db");
+        $conn = mysqli_connect("******.my3w.com","******","******","******_db");
         mysqli_query($conn,"SET NAMES utf8");
         return $conn;
     }
     
     public static function opearting($sql){
         return mysqli_query(self::getConn(),$sql);
+        
     }
 }
 
