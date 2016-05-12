9768f1e33a52de50adb2816837ccf9357d80696c
eary <yy.liu@foxmail.com>
2016-05-12 12:05:24 +0800

    add jsonp interface to json

--- bing/json/index.php
+++ bing/json/index.php
@@ -9,7 +9,12 @@ header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
 header('content-type: application/json; charset=utf-8');
 require('../../lib/base.php');
+$callback = $_REQUEST['callback'];
 $base = new Base();
 $array = $base->getLastPic();
-echo json_encode($array);
+if($callback){
+    echo $callback.'('.json_encode($array).')';
+}else{
+    echo json_encode($array);
+}
 ?>
