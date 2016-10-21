1c9a274acd226e57e0f8d18d10f68a41c97550e6
Dotos <yy.liu@foxmail.com>
2016-10-21 18:14:43 +0800

    add index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: '必应壁纸'
+        , description: 'Bing 每日壁纸'
+    });
 });
-
 module.exports = router;
