91b71eb5a2eb9321f008decd963682d9dd981d08
dotos <yy.liu@foxmail.com>
2016-10-30 18:13:39 +0800

    add v1 rand apis

---
+++ routes/v1.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var dbUtils = require('../utils/dbUtils');
+var qiniuUtils = require('../utils/qiniuUtils');
+var cookie = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36' };
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/rand', function(req, res, next) {
+    dbUtils.getCount('bing', {}, function(rows) {
+        if (rows.length > 0) {
+            var sum = Number(rows[0]);
+            var rand = Math.random() * (sum - 1) + 1;
+            dbUtils.get('bing', {
+                id: rand
+            }, function(rs) {
+                var data = rs[0];
+                var qiniu_url = data.qiniu_url;
+                var fullURL = qiniuUtils.imageView(qiniu_url);
+                console.log(fullURL);
+                request.get(fullURL)
+                    .set(cookie)
+                    .end(function(err, response) {
+                        res.header('content-type', 'image/jpg');
+                        res.send(response.body);
+                    });
+            })
+        }
+    });
+})
+
+module.exports = router;
--- utils/qiniuUtils.js
+++ utils/qiniuUtils.js
@@ -1,4 +1,5 @@
 var qiniu = require('qiniu');
+var request = require('super-agent');
 // access_key and secret_key
 qiniu.conf.ACCESS_KEY = process.env.qiniu_access_key;
 qiniu.conf.SECRET_KEY = process.env.qiniu_secret_key;
@@ -46,5 +47,30 @@ module.exports = {
             });
         }
         callback && callback();
+    },
+    /**
+     * 创建预览图片
+     * @param {String} url      图片名称
+     * @param {Number} width    缩略图宽度(默认1920)
+     * @param {Number} height   缩略图高度(默认1080)
+     * @param {Number} quality  图片质量(0-100,默认100)
+     * @param {Number} mode     缩略图处理模式(默认1)
+     *                              1:表示限定目标缩略图的宽度和高度，放大并从缩略图中央处裁剪为指定 <Width>x<Height> 大小的图片。
+     *                              2:指定 <Width> 和 <Height>，表示限定目标缩略图的长和宽，将缩略图的大小限定在指定的宽高矩形内。
+     *                              2:指定 <Width> 但不指定 <Height>，表示限定目标缩略图的宽度，高度等比缩略自适应。
+     *                              2:指定 <Height> 但不指定 <Width>，表示限定目标缩略图的高度，宽度等比缩略自适应。
+     * @return {String} fullURL
+     * 
+     */
+    imageView: function(url, width, height, quality, mode) {
+        width = width || 1920;
+        height = height || 1080;
+        quality = quality || 100;
+        mode = mode || 1;
+        var base = 'http://7xilig.com1.z0.glb.clouddn.com/bing/';
+        url = url.indexOf('1920x1080') == -1 ? url + '_1920x1080.jpg' : url;
+        url = base + url;
+        var imageView = new qiniu.fop.ImageView(mode, width, height, quality);
+        return imageView.makeRequest(url);
     }
 };
