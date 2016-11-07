80a3e21a3126999082b4eea9d6aabdf3c465177c
Dotos <yy.liu@foxmail.com>
2016-11-07 14:57:05 +0800

    add new api at v1

--- routes/v1.js
+++ routes/v1.js
@@ -6,10 +6,38 @@ var cookie = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWeb
 
 var router = express.Router();
 
-/* GET users listing. */
+/**
+ * v1 默认返回最新的bing壁纸信息
+ */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    commonV1(req, res, next);
 });
+router.post('/', function(req, res, next) {
+    commonV1(req, res, next);
+});
+
+var commonV1 = function(req, res, next) {
+    dbUtils.get('bing', {
+        page: {
+            no: 1,
+            size: 1
+        },
+        body: {}
+    }, function(rows) {
+        if (rows.length > 0) {
+            if (req.method === 'GET' && req.query.callback) {
+                res.jsonp(rows[0]);
+            } else {
+                res.json(rows[0]);
+            }
+        } else {
+            res.json({
+                status: -1,
+                message: '很抱歉，由于未知原因，暂时无法提供服务，请稍后再试!'
+            });
+        }
+    });
+}
 
 router.get('/rand', function(req, res, next) {
     dbUtils.getCount('bing', {}, function(rows) {
