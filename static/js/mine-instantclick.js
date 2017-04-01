069f7b2a2f9cee7ab5fc4f142fe7fdb6151c467d
xCss <yy.liu@foxmail.com>
2017-04-01 23:26:47 +0800

    fix support baidu analysis

--- static/js/mine-instantclick.js
+++ static/js/mine-instantclick.js
@@ -1,9 +1,11 @@
-InstantClick.on('change', function(isInitialLoad) {
-    if (isInitialLoad === false) {
-        if (typeof _hmt !== 'undefined')  // support 百度统计
-            _hmt.push(['_trackPageview', location.pathname + location.search]);
-        if (typeof ga !== 'undefined')  // support google analytics
-            ga('send', 'pageview', location.pathname + location.search);
-    }
+$(function(){
+    InstantClick.on('change', function(isInitialLoad) {
+        if (isInitialLoad === false) {
+            if (typeof _hmt !== 'undefined')  // support 百度统计
+                _hmt.push(['_trackPageview', location.pathname + location.search]);
+            if (typeof ga !== 'undefined')  // support google analytics
+                ga('send', 'pageview', location.pathname + location.search);
+        }
+    });
+    InstantClick.init();
 });
-InstantClick.init();
