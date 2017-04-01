737f80ac1fa6350c055ba02ef60ca3abf4574ec1
xCss <yy.liu@foxmail.com>
2017-04-01 23:22:40 +0800

    fix support baidu analysis

--- static/js/baidu-analysis.js
+++ static/js/baidu-analysis.js
@@ -1,7 +1,7 @@
 if (!/^http:\/\/localhost/.test(location.href)) {
     var _hmt = _hmt || [];
     var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?667639aad0d4654c92786a241a486361";
+    hm.src = "//hm.baidu.com/hm.js?667639aad0d4654c92786a241a486361&r="+Math.random();
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
     //_hmt.push(['_trackPageview', location.pathname + location.search]);
