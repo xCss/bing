b6f202ba2fb06a05ee6ddb6aaf75814011d62418
dotos <yy.liu@foxmail.com>
2017-08-19 16:02:59 +0800

    fix

--- utils/weiboUtils.js
+++ utils/weiboUtils.js
@@ -96,6 +96,7 @@ module.exports = {
             }
             request
                 .post(share)
+                .set('Content-Type', 'application/json')
                 //.type('form')
                 .set(cookie)
                 .send(post)
